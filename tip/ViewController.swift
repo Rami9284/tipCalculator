b575e0c63576cedad4f1d1ade53c47675154d4ce
Judith Ramirez Cortez <rami9284@m506-118-26425.local>
2019-01-23 15:45:58 -0800

    calc commit

--- tip/ViewController.swift
+++ tip/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tiplabel: UILabel!
+    
+    @IBOutlet weak var totlabel: UILabel!
+    
+    @IBOutlet weak var billField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func calculatorTip(_ sender: Any) {
+        //get bill amt
+        let bill = Double(billField.text!) ?? 0
+        //calc tip & tot
+        let tip = bill * 0.1
+        let total = bill + tip
+        //update tip & tot labels
+        
+        tiplabel.text = String(format: "$%.2f",tip)
+        totlabel.text = String(format: "$%.2f",total)
+    }
 }
 
